06bffbcf4110e206cf939a4e1ccd76417374ef59
kim-hyunjin <hyunjin1612@gmail.com>
2022-07-26 23:46:58 +0900

    feat: add documentation

--- server/index.js
+++ server/index.js
@@ -29,16 +29,34 @@ let tweets = [
 // SDL (Schema Definition Language)
 const typeDefs = gql`
   type Query {
+    """
+    Get all the Tweets
+    """
     allTweets: [Tweet!]!
+    """
+    Get Tweet with id
+    """
     tweet(id: ID!): Tweet
+    """
+    Get all the Users
+    """
     allUsers: [User!]!
   }
 
   type Mutation {
+    """
+    Create a Tweet, and return created Tweet
+    """
     postTweet(text: String!, userId: ID!): Tweet!
+    """
+    Deletes a Tweet if found, else returns false
+    """
     deleteTweet(id: ID!): Boolean!
   }
 
+  """
+  Tweet object represents a resource for a tweet
+  """
   type Tweet {
     id: ID!
     text: String!
@@ -49,6 +67,9 @@ const typeDefs = gql`
     id: ID!
     firstName: String!
     lastName: String
+    """
+    Is the sum of first name + last name as a string
+    """
     fullname: String!
   }
 `;
