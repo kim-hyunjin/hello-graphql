27003aa96a6fccf7e57815cdb909d50254ea0b65
kim-hyunjin <hyunjin1612@gmail.com>
2022-07-26 22:52:23 +0900

    feat: config server, run server, add typeDef

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import { ApolloServer, gql } from 'apollo-server';
+
+// SDL (Schema Definition Language)
+const typeDefs = gql`
+  type Query {
+    allTweets: [Tweet]
+    tweet(id: ID): Tweet
+  }
+
+  type Tweet {
+    id: ID
+    text: String
+    author: User
+  }
+
+  type User {
+    id: ID
+    username: String
+  }
+`;
+
+const server = new ApolloServer({ typeDefs });
+
+server.listen().then(({ url }) => {
+  console.log(`Running on ${url}`);
+});
